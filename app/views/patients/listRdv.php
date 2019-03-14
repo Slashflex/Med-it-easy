70e1b8ed8e7d03733f402f82e5748cd7aa768671
Slashflex <pro.davidsaoud@gmail.com>
2019-03-14 23:00:13 +0100

    update patients event added + shorcodes

--- app/views/patients/listRdv.php
+++ app/views/patients/listRdv.php
@@ -7,22 +7,21 @@
         <h4>Bienvenue <?= ucfirst($_SESSION['patientPrenom']) . ' ' . ucfirst($_SESSION['patientNom']) . ' '; ?>
         </h4>
     </div>
-    <p class="text-center">Vous êtes sur la page de gestion de votre profil, d'ici vous pourrez voir vos
+    <p class="text-center selectType">Vous êtes sur la page de gestion de votre profil, d'ici vous pourrez voir vos
         consultations et gérer votre profil.</p>
-
     <div class="row">
-        <h5 class="">Vos consultations à venir :</h5>
+        <h5 class="mx-auto mt-3 mb-4 frame-title col-lg-12">Vos consultations à venir :</h5>
+        <a href="index.php?action=updateRdv" class="mx-auto editBtn"><i class="far fa-edit"></i> Modifier</a> 
         <?php while ($data = $listRdv->fetch()): ?>
-        <p class="text-center" style="width: 100%">
-            <?= ' ' . $data['start'] . ' à ' . $data['hour'] . 
-                ' avec le Docteur ' . $data['praticienPrenom'] . 
-                ' ' . $data['praticienNom']. '</br>' ?>
+        <p class="text-center col-md-12 selectType" style="width: 100%; border-bottom: 1px solid #dbae58"><hr>
+            <?= '<span id="colorRdv" style="background-color:' . $data['couleur'] . '">' . $data['description'] . '</span>Le ' . $data['start'] .
+                ' à ' . $data['hour'] .
+                ' avec le Docteur ' . ucfirst($data['praticienPrenom']) .
+                ' ' . ucfirst($data['praticienNom']);  ?>
         </p>
         <?php endwhile; ?>
-
     </div>
 </div>
-
 
 <?php $content = ob_get_clean(); ?>
 
