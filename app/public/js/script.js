41b6ef00dcf7b637a25b729689b8b3b60fadc122
Slashflex <pro.davidsaoud@gmail.com>
2019-03-20 22:11:17 +0100

    GDPR script added

--- app/public/js/script.js
+++ app/public/js/script.js
@@ -189,10 +189,32 @@ $(function () {
 /*=============================================================
 ==========================  TESTS   ===========================
 =============================================================*/
-$(function() {
-    $('a[href*=#]').on('click', function(e) {
-      e.preventDefault();
-      $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 2000, 'linear');
-    });
-  });
+$(document).ready(function(){
+	if ($.cookie('cookies_enable')) {
+		$("#message").css("display:","none");
+    } else {
+		$('#message').delay(1000).slideDown(500);
+		}
+});
+
+// To hide the message but show it again on page reload
+function hideMessage(){
+  $('#message').delay().slideUp(500);
+}
+
+// To hide the message and set the cookie so when the page is reloaded the message does not appear
+function hideForever(){
+  $('#message').delay().slideUp(500);
+  $.cookie('cookies_enable', 'foo', { expires: 180 });
+}
+
+// Optional function to show message manually on the demo page
+function showMessage(){
+  $('#message').delay().slideDown(500);
+}
+
+// Delete cookie
+function deleteCookie() {
+  $.removeCookie('cookies_enable');
+}
 /*======================  End of TESTS  =====================*/
