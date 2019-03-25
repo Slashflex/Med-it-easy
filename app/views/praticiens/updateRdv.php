386e8616e04aeb2dbb4f7016736decc47f6f474a
Slashflex <pro.davidsaoud@gmail.com>
2019-03-25 23:59:31 +0100

    This is a new commit for what I originally planned to be amended

--- app/views/praticiens/updateRdv.php
+++ app/views/praticiens/updateRdv.php
@@ -2,6 +2,15 @@
 
 <?php ob_start(); ?>
 
+<?php while ($data = $req->fetch()): ?>
+<div class="row pt-5 mt-5" style="background-color:<?= $data['couleur']; ?>">
+    <p class="toDo"><?= $data['id_event']; ?></p>
+    <p class="toDo"><?= $data['description']; ?></p>
+    <p class="toDo"><?= ucfirst($data['patientPrenom']) . ' ' . ucfirst($data['patientNom']); ?></p>
+    <p class="toDo"><?= $data['start']; ?></p>
+    <p class="toDo"><?= $data['hour']; ?></p>
+</div>
+<?php endwhile; ?>
 
 
 <?php $content = ob_get_clean(); ?>
