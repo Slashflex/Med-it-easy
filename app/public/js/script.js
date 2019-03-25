0272afe098bdb197e72c88a6bbb469d9b6ed8356
Slashflex <pro.davidsaoud@gmail.com>
2019-03-25 08:36:51 +0100

    GDPR added

--- app/public/js/script.js
+++ app/public/js/script.js
@@ -190,14 +190,14 @@ $(function () {
     if ($.cookie('cookies_enable')) {
         $("#message").css("display:", "none");
     } else {
-        $('#message').delay(5000).slideDown(500);
+        $('#message').delay(1000).slideDown(500);
     }
 });
 
 // To hide the message but show it again on page reload
-function hideMessage() {
-    $('#message').delay().slideUp(500);
-}
+// function hideMessage() {
+//     $('#message').delay().slideUp(500);
+// }
 
 // To hide the message and set the cookie so when the page is reloaded the message does not appear
 function hideForever() {
@@ -208,14 +208,14 @@ function hideForever() {
 }
 
 // Optional function to show message manually on the demo page
-function showMessage() {
-    $('#message').delay().slideDown(500);
-}
+// function showMessage() {
+//     $('#message').delay().slideDown(500);
+// }
 
 // Delete cookie
-function deleteCookie() {
-    $.removeCookie('cookies_enable');
-}
+// function deleteCookie() {
+//     $.removeCookie('cookies_enable');
+// }
 // --- /. GDPR POP UP
 
 
