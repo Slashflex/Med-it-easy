97d9cdb379c23b3a93459f597beb5347deca89b4
Slashflex <pro.davidsaoud@gmail.com>
2019-03-26 09:03:36 +0100

    This is a new commit for what I originally planned to be amended

--- app/views/praticiens/agendaAdmin.php
+++ app/views/praticiens/agendaAdmin.php
@@ -13,7 +13,8 @@
             <h4>Bienvenue Docteur <?= ucfirst($_SESSION['praticienPrenom']) . ' ' . ucfirst($_SESSION['praticienNom']); ?>
             </h4>
         </div>
-        <p class="text-center margin selectType">Vous êtes sur la page de gestion de votre profil, d'ici vous pourrez voir et
+        <p class="text-center margin selectType">Vous êtes sur la page de gestion de votre profil, d'ici vous pourrez
+            voir et
             gérer vos
             consultations.</p>
         <div id="calendar"></div>
@@ -24,7 +25,7 @@
         <div class="dateNav mx-auto"><?php setlocale(LC_ALL, 'fr_FR'); ?><?=  ucfirst(strftime("%A %e %B %Y", mktime())) . '<br>' . strftime("%H : %M", mktime());?>
         </div>
         <ul class="menu mx-auto offMenu">
-        <li><a href="fr/accueil"><i class="fas fa-home fa-1x"></i> Accueil</a></li>
+            <li><a href="fr/accueil"><i class="fas fa-home fa-1x"></i> Accueil</a></li>
             <li><a href="fr/agenda"><i class="far fa-calendar-alt fa-1x"></i> Agenda</a></li>
             <li><a href="fr/tarifs"><i class="fas fa-hand-holding-usd fa-1x"></i> Tarifs</a></li>
             <li><a href="fr/horaires"><i class="far fa-clock fa-1x"></i> Horaires</a></li>
@@ -32,7 +33,7 @@
             <li><a href="fr/suppression-du-compte"><i class="fas fa-sign-out-alt"></i> Supprimer mon compte</a></li>
         </ul>
     </nav>
-</div>
+</div><!-- end: OffMenu Canva -->
 
 <?php $content = ob_get_clean(); ?>
 
